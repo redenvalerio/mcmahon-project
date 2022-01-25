4878efebc4c3acfc21fb51d2950bd6bc75297bd7
redenvalerio <redenvalerio2@gmail.com>
2022-01-26 05:16:44 +0800

    added contact

--- pages/servicearea.js
+++ pages/servicearea.js
@@ -1,11 +1,12 @@
 import { Box, Heading, Text, Flex } from "@chakra-ui/react"
+import ServiceArea from "../components/ServiceArea"
 
 export default function Index() {
   return (
     <Flex p="8" wrap="wrap" align="center" direction="column">
       <Box h="100vh">
         <Heading>
-          Service Area
+          <ServiceArea/>
         </Heading>
       </Box>
     </Flex>

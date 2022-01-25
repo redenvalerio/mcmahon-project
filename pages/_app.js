5868c885617655daef4936f14ff5b3bac0623c81
redenvalerio <redenvalerio2@gmail.com>
2022-01-25 20:47:30 +0800

    Improved Navbar

--- pages/_app.js
+++ pages/_app.js
@@ -2,11 +2,12 @@ import { ChakraProvider, Box} from "@chakra-ui/react"
 import Navbar from "../components/Navbar"
 import Footer from "../components/Footer"
 import { FloatingSwitch } from "../components/FloatingSwitch"
+import NavbarImproved from "../components/NavbarImproved"
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider>
-      <Navbar/>
+      <NavbarImproved/>
       <Component {...pageProps} />
       <Footer/>
       <FloatingSwitch/>
---
+++ pages/serviceoffered.js
@@ -0,0 +1,13 @@
+import { Box, Heading, Text, Flex } from "@chakra-ui/react"
+
+export default function Index() {
+  return (
+    <Flex p="8" wrap="wrap" align="center" direction="column">
+      <Box h="100vh">
+        <Heading>
+          Services Offered
+        </Heading>
+      </Box>
+    </Flex>
+  )
+}

266998f766cac9cfe8c724665a5c94dc575b1380
redenvalerio <redenvalerio2@gmail.com>
2022-01-25 13:28:09 +0800

    Added navigation bar

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,14 +1,17 @@
-import { HStack, Box, Text, Flex, Link, Spacer, Button, Image } from "@chakra-ui/react"
+import { useColorMode, HStack, Box, Text, Flex, Link, Spacer, Button, Img} from "@chakra-ui/react"
+import Image from "next/image"
 import NextLink from "next/link"
 import DarkModeSwitch from "../components/DarkModeSwitch"
-import Logo from "../public/mcmahon-logo.png"
 
 export default function Navbar() {
+	const { colorMode } = useColorMode()
+	const LightModeLogo = "mcmahon-logo.png"
+	const DarkModeLogo = "mcmahon-logo-dark.png"
 	return (
 		<Flex p="5">
-			<Image src={Logo} boxSize="150px"/>
+			<Img ml="10" w="20%" src={colorMode === 'dark' ? DarkModeLogo : LightModeLogo} />
 			<Spacer/>
-			<Flex>
+			<Flex mb="10">
 				{/* Navbar links */}
 				<HStack>
 					<NextLink href="/">

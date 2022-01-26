b3cfeecb99d711183ead87ca7c5ffabdcb2c2e39
SDPT Solutions <sdptincorporated@gmail.com>
2022-01-26 14:59:20 +0800

    Ayaw ma Commit potek na 3D yan

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,7 @@ import Navbar from "../components/Navbar"
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider resetCSS>
+      <script type="module" src="https://unpkg.com/@google/model-viewer/dist/model-viewer.min.js"></script>
       <Navbar/>
       <Component {...pageProps} />
       <Footer/>
